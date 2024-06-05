27f3c292bd448d7d9fe68ed4a616ecd16960f55b
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-06-05 00:21:22 +0000

    add mock rules function

--- intro.py
+++ intro.py
@@ -12,3 +12,18 @@ __        __   _                            _          _   _
 {Style.RESET_ALL}
 """
     print(logo)
+# function to display rules
+def display_rules():
+    rules = f"""
+{Fore.YELLOW}Welcome to Astro Hangman Game!{Style.RESET_ALL}
+{Fore.GREEN}
+Rules:
+1. Choose a difficulty level: 1, 2, or 3.
+2. You will be given a word related to space.
+3. Guess the word one letter at a time.
+4. You can make a limited number of wrong guesses based on the chosen level.
+5. The spaceship's battery level will indicate your remaining guesses.
+6. Guess the word before you run out of guesses to win the game!
+{Style.RESET_ALL}
+"""
+    print(rules)
